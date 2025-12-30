1da08e8615d098f7b344dfed6544d95a5fa846dc
Juan Escalante <jcec007@gmail.com>
2025-12-29 22:42:19 -0500

    New: Service Status endpoint

---
+++ Icomments/Controllers/ServiceStatusController.cs
@@ -0,0 +1,22 @@
+﻿using Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Icomments.Controllers
+{
+    [Route("api/icomments/v1/service-status")]
+    [ApiController]
+    public class ServiceStatusController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> GetStatus([FromQuery] UrlRequestBase? urlRequestBase)
+        {
+            int status = 200;
+            object? response = null;
+
+            return StatusCode(status, await ResponseBase.Response(response));
+
+        }
+    }
+}
